f7dee7ec3e6dbe86720ee7a5d7ca883d074ca2ec
Sidhesh004 <2023BIT004@sggs.ac.in>
2025-02-02 22:22:52 +0530

    Update Practical7.java

--- Practical7.java
+++ Practical7.java
@@ -52,7 +52,7 @@ class Practical7 {
         int n= 5;
         for(int row = 1; row<=n; row++){
             for(int col = 1; col <=n; col++){
-                if (col == 1 || row == 1 && col<=n-1 || row == 5 && col<=n-1 || ((row>=2&row<=4)&col == 5)) {
+                if (col == 1|| col==5 || row==3) {
                     System.out.print("*");
                 }
                 else{
